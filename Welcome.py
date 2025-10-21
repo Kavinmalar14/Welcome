a46d05390b1e073bb128b73521eace4ee3d3c073
Kavinmalar14 <parthibanpalaniappan@gmail.com>
2025-10-21 18:52:06 +0100

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,13 @@
+#Write a program for print command.
+
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguements for printing
+print("Hello" ,5)
